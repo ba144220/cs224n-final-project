66ed17e9a111e427cde46fbf2e0c4523e2c4d734
ba144220 <b08901097@ntu.edu.tw>
2025-02-24 04:22:19 +0800

    feat: init soft prompt

--- src/models/llama_prompt_tuning.py
+++ src/models/llama_prompt_tuning.py
@@ -1,4 +1,5 @@
 from transformers import LlamaForCausalLM, LlamaConfig
+
 from typing import Tuple, Optional
 
 import torch
@@ -25,10 +26,51 @@ class LlamaPromptTuningLM(LlamaForCausalLM):
         
         if config.prompt_tuning_range is not None:
             soft_prompt_len = config.prompt_tuning_range[1] - config.prompt_tuning_range[0]
-            # TODO: Initialize the soft prompt with random values
             self.soft_prompt = nn.Parameter(torch.zeros(soft_prompt_len, config.hidden_size))
         else:
             self.soft_prompt = None
+    
+    def get_soft_prompt(self):
+        return self.soft_prompt
+    
+    def reset_soft_prompt(
+        self, 
+        prompt_tuning_range: Tuple[int, int],
+    ):
+        print(f"Resetting soft prompt. You might want to initialize the soft prompt again.")
+        self.config.prompt_tuning_range = prompt_tuning_range
+        self.soft_prompt = nn.Parameter(torch.zeros(prompt_tuning_range[1] - prompt_tuning_range[0], self.config.hidden_size, device=self.device))
+    
+    def init_soft_prompt_with_zero(self):
+        assert self.soft_prompt is not None, "Soft prompt is not initialized"
+        self.soft_prompt.data.zero_()
+    
+    def init_soft_prompt_with_random_values(
+        self,
+        mean: float = 0.0,
+        std: float = 0.02,
+    ):
+        assert self.soft_prompt is not None, "Soft prompt is not initialized"
+        self.soft_prompt.data.normal_(mean, std)
+    
+    def init_soft_prompt_with_prompt_embedding(
+        self,
+        token_ids: torch.Tensor,
+    ):
+        """
+        token_ids: int torch.Tensor with shape (seq_len)
+        """
+        assert self.soft_prompt is not None, "Soft prompt is not initialized"
+        # Sanity check
+        assert token_ids.ndim == 1, "token_ids must be a 1D tensor"
+        # Check the length of the token ids
+        soft_prompt_len = self.config.prompt_tuning_range[1] - self.config.prompt_tuning_range[0]
+        assert token_ids.size(0) == soft_prompt_len, "The length of the token ids must be equal to the length of the soft prompt"
+        
+        # Get the prompt embedding
+        prompt_embedding = self.model.embed_tokens(token_ids)
+        self.soft_prompt.data.copy_(prompt_embedding)
+        
 
     def forward(
         self,
@@ -36,7 +78,11 @@ class LlamaPromptTuningLM(LlamaForCausalLM):
         inputs_embeds: Optional[torch.Tensor] = None,
         **kwargs,
     ) -> torch.Tensor:
-        if self.soft_prompt is not None and self.config.prompt_tuning_range is not None:
+        if self.config.prompt_tuning_range is not None:
+            assert self.soft_prompt is not None, "Soft prompt is not initialized"
+        
+        if self.config.prompt_tuning_range is not None:
+            # Get the input embeddings
             if inputs_embeds is None:
                 inputs_embeds = self.model.embed_tokens(input_ids)
             
@@ -47,4 +93,5 @@ class LlamaPromptTuningLM(LlamaForCausalLM):
                                 
             return super().forward(inputs_embeds=inputs_embeds, **kwargs)
         else:
+            # No prompt tuning
             return super().forward(input_ids=input_ids, inputs_embeds=inputs_embeds, **kwargs)
