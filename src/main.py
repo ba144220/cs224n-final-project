6d5a4d73cf320da59bad9f446b506dd98a306914
Andy Ouyang <andy136899@gmail.com>
2025-02-24 00:06:02 -0800

    added superglue and gsm8k

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM
 import lovely_tensors
+from data.superglue import load_and_process_superglue
+from data.gsm8k import load_and_process_gsm8k
 
 lovely_tensors.monkey_patch()
 
@@ -18,3 +20,15 @@ print(f"{model.model.embed_tokens.weight[:,0] = }")
 # Print out the embedding
 
 
+# Load and process example datasets
+task = "boolq"
+print(f"Processing SuperGLUE dataset: {task}")
+dataset = load_and_process_superglue(task, tokenizer)
+
+task = "gsm8k"
+print(f"Processing dataset: {task}")
+gsm8k_dataset = load_and_process_gsm8k(tokenizer)
+
+# Print out a sample
+print(f"Processed GSM8K example: {gsm8k_dataset['train'][0]}")
+print(f"Processed dataset example: {dataset['train'][0]}")
